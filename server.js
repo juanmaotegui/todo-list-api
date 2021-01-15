f2f723b761ad93bceed249069697dcc3c8b38740
Juan Manuel Otegui <juanmaotegui@gmail.com>
2021-01-14 23:00:05 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("./connections");
+
+var express = require("express"),
+	app = express(),
+	port = process.env.PORT || 3000,
+	mongoose = require("mongoose"),
+	Task = require("./api/models/todoListModel"), //created model loading here
+	bodyParser = require("body-parser");
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(global.MONGO_URL);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require("./api/routes/todoListRoutes"); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+// En caso de de una url que no existe, retornamos mensaje propio
+app.use(function (req, res) {
+	res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+console.log("todo list RESTful API server started on: " + port);
